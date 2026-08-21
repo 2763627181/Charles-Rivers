4d689b3f6bbaa96dab2e6e8fa1cf0bedbe2baa4b
Joshua Williams <joshuasteven486@gmail.com>
2026-08-21 01:55:29 -0400

    Swap hero mice trio photo for new provided image
    New photo has transparent corners that blend into the hero gradient.

--- components/home/hero-section.tsx
+++ components/home/hero-section.tsx
@@ -74,7 +74,7 @@ export function HeroSection() {
           className="mx-auto w-full max-w-xl"
         >
           <Image
-            src="/images/reference/home/hero-mice-trio.jpg"
+            src="/images/reference/home/hero-mice-trio.png"
             alt="Black, agouti and white laboratory mice used throughout this reference guide"
             width={1536}
             height={1024}
